e58edc937134d12a8e55f1e61ae7dceb94d9d321
Shifuuuuuuuu <ron.sanhueza@duocuc.cl>
2024-09-25 16:36:16 -0300

    Actualización de proyecto
    se agregar la conexión de la base de datos

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAAZbKeKyOlMbRDG36TfY8WhJ0Tv80gCRw",
+    authDomain: "generador-reportes.firebaseapp.com",
+    projectId: "generador-reportes",
+    storageBucket: "generador-reportes.appspot.com",
+    messagingSenderId: "908863305170",
+    appId: "1:908863305170:web:e43e9ab7215db268c6c40b"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAAZbKeKyOlMbRDG36TfY8WhJ0Tv80gCRw',
+    authDomain: 'generador-reportes.firebaseapp.com',
+    projectId: 'generador-reportes',
+    storageBucket: 'generador-reportes.appspot.com',
+    messagingSenderId: '908863305170',
+    appId: '1:908863305170:android:e43e9ab7215db268c6c40b',
+    measurementId: 'G-XXXXXXXXXX' // Reemplaza con el Measurement ID si lo tienes
+  }
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
